6420895269fce1744edfe4387cc2658ddae00896
Fluffy <126976270+TheSuperFluffyCat@users.noreply.github.com>
2025-02-14 10:42:01 +0100

    Language Update ReloadCommand.java
    Wrong language 😅

--- src/main/java/org/fliff/lifeSteal/commands/ReloadCommand.java
+++ src/main/java/org/fliff/lifeSteal/commands/ReloadCommand.java
@@ -19,7 +19,7 @@ public class ReloadCommand implements CommandExecutor, TabCompleter {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player) || sender.hasPermission("lifesteal.reload")) {
             LifeSteal.getInstance().reloadConfig();
-            configManager.reloadConfig(); // Konfiguration neu laden!
+            configManager.reloadConfig(); 
             sender.sendMessage("§x§4§E§F§B§0§7✅ §x§5§D§E§6§0§8L§x§6§D§D§2§0§8i§x§7§C§B§D§0§9f§x§8§C§A§9§0§Ae§x§9§B§9§4§0§AS§x§A§A§8§0§0§Bt§x§B§A§6§B§0§Ce§x§C§9§5§7§0§Da§x§D§9§4§2§0§Dl §x§E§8§2§E§0§EC§x§F§7§1§9§0§Fo§x§F§F§1§7§1§7n§x§F§F§2§6§2§6f§x§F§F§3§6§3§6i§x§F§F§4§5§4§5g §x§F§F§5§5§5§5R§x§F§F§6§5§6§5e§x§F§F§7§4§7§4l§x§F§F§8§4§8§4o§x§F§F§9§3§9§3a§x§F§F§A§3§A§3d§x§F§F§B§2§B§2e§x§F§F§C§2§C§2d");
         }
         return true;
